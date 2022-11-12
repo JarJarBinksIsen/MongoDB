cf2e0b731a57d5a7c811bb231266c4e5cd6fa069
JarJarBinksIsen <36074939+JarJarBinksIsen@users.noreply.github.com>
2022-11-12 12:43:56 +0100

    UpdateMany & separe exos en plusieurs files

--- exo1.py
+++ exo1.py
@@ -23,7 +23,7 @@ cities_urls = {
     "paris" : "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&rows=3000&facet=etatconnexion"
 }
 
-
+# Lille
 vlilles = get_veloStations_by_city(cities_urls["lille"])
 vlilles_to_insert = [
     {
@@ -40,10 +40,11 @@ vlilles_to_insert = [
     for elem in vlilles
 ]
 try: 
-    db.stations.insert_many(vlilles_to_insert, ordered=False)
+    db.stations.update_many({}, vlilles_to_insert, upsert=True)
 except:
     pass
 
+#Paris
 velibs = get_veloStations_by_city(cities_urls["paris"])
 velibs_to_insert = [
     {
@@ -60,46 +61,6 @@ velibs_to_insert = [
     for elem in velibs
 ]
 try: 
-    db.stations.insert_many(velibs_to_insert, ordered=False)
+    db.stations.update_many({}, velibs_to_insert, upsert=True)
 except:
     pass
-
-# exercice 2
-while True:
-    print('update')
-    vlilles = get_veloStations_by_city(cities_urls["lille"])
-    lille_datas = [
-        {
-            "bike_available": elem.get('fields', {}).get('nbvelosdispo'),
-            "stand_available": elem.get('fields', {}).get('nbplacesdispo'),
-            "date": dateutil.parser.parse(elem.get('fields', {}).get('datemiseajour')),
-            "station_id": elem.get('fields', {}).get('libelle')
-        }
-        for elem in vlilles
-    ]
-    
-    for data in lille_datas:
-        last_station_update = db.datas.find({ '$and': [{ "station_id":{'$exists':True}},{ "station_id":data["station_id"]}]}).sort("date", -1).limit(1)[0]
-        #print(last_station_update["station_id"])
-        if(last_station_update["bike_available"] != data["bike_available"] or last_station_update["stand_available"] != data["stand_available"]):
-            db.datas.insert_one(data)
-        db.datas.update_one({'date': data["date"], "station_id": data["station_id"]}, { "$set": data }, upsert=True)
-
-    velibs = get_veloStations_by_city(cities_urls["paris"])
-    paris_datas = [
-        {
-            "bike_available": elem.get('fields', {}).get('numbikesavailable'),
-            "stand_available": elem.get('fields', {}).get('numdocksavailable'),
-            "date": dateutil.parser.parse(elem.get('fields', {}).get('duedate')),
-            "station_id": elem.get('fields', {}).get('stationcode')
-        }
-        for elem in velibs
-    ]
-    
-    for data in paris_datas:
-        last_station_update = db.datas.find({ '$and': [{ "station_id":{'$exists':True}},{ "station_id":data["station_id"]}]}).sort("date", -1).limit(1)[0]
-        if(last_station_update["bike_available"] != data["bike_available"] or last_station_update["stand_available"] != data["stand_available"]):
-            db.datas.insert_one(data)
-        db.datas.update_one({'date': data["date"], "station_id": data["station_id"]}, { "$set": data }, upsert=True)
-
-    time.sleep(10)
